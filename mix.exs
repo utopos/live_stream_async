a0316065aebaac0873f290a8009e6e25180eed53
Kuba <23064435+utopos@users.noreply.github.com>
2024-08-12 16:32:06 +0700

    Add support for Elixir 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LiveStreamAsync.MixProject do
     [
       app: :live_stream_async,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: ">= 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
